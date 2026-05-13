74f8e3c908ff1f92fca97a259afc4989b56c83ef
Oscar Öjling <oscarojling@gmail.com>
2026-05-13 13:16:58 +0200

    Changed h3 to h2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = ({ title, subtitle }: HeaderProps) => {
   return (
     <header className="flex flex-col p-6 text-center gap-1">
       <h1 className="font-primary text-5xl md:text-7xl">{title}</h1>
-      <h3 className="font-secondary text-tertiary text-lg">{subtitle}</h3>
+      <h2 className="font-secondary text-tertiary text-2xl">{subtitle}</h2>
     </header>
   );
 };
